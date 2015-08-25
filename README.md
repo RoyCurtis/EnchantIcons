8ac9c81ccbe05034aa56f56c5105c56e3e2de8f4
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2015-08-25 16:10:29 +0100

    * licensed under 3-clause BSD (as agreed by KJ4IPS)
    * added KJ4IPS as author under mcmod.info
    * updated gradlefile, README and EnchantIcons to latest conventions
    * enforce siding; mod will now only work client side. fixes crash on server side

---
+++ README.md
@@ -0,0 +1,14 @@
+[![Build Status](http://ci.haun.guru/buildStatus/icon?job=EnchantIcons)](http://ci.haun.guru/job/EnchantIcons/)
+
+**EnchantIcons** is a client-only mod that makes enchanted books easier to find, based on their inner enchantment. It is available for 1.7.10 and 1.6.4.
+
+This is the 1.6.4 branch, `16backport`.
+
+# Building
+
+## Requirements
+
+* [Gradle installation with gradle binary in PATH](http://www.gradle.org/installation). Unlike the source package provided by Forge, this repository does not include a gradle wrapper or distribution.
+
+## Usage
+Simply execute `gradle setupCIWorkspace` in the root directory of this repository. Then execute `gradle build`. If subsequent builds cause problems, do `gradle clean`.
--- src/main/java/roycurtis/EnchantIcons.java
+++ src/main/java/roycurtis/EnchantIcons.java
@@ -4,39 +4,49 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.MinecraftForgeClient;
 
-import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@Mod(modid = EnchantIcons.MODID, version = EnchantIcons.VERSION)
+@Mod(
+    modid   = EnchantIcons.MODID,
+    name    = EnchantIcons.MODID,
+    version = EnchantIcons.VERSION
+)
 public class EnchantIcons
 {
-    public static final String MODID   = "enchanticons";
+    /** Frozen at 0.0.1 to prevent misleading world save error */
     public static final String VERSION = "0.0.1";
+    public static final String MODID   = "enchanticons";
 
-    public static Logger logger;
-    public static File   BaseDir;
+    public static Logger LOG;
     public static EnchantIconsConfiguration config;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
+    @SideOnly(Side.SERVER)
+    public void serverPreInit(FMLPreInitializationEvent event)
     {
-        logger  = event.getModLog();
-        BaseDir = new File(event.getModConfigurationDirectory(), MODID);
-        config = new EnchantIconsConfiguration(event.getSuggestedConfigurationFile());
-
-
-        if ( !BaseDir.exists() )
-            BaseDir.mkdir();
+        LOG = event.getModLog();
+        LOG.severe("This mod is intended only for use on clients");
+        LOG.severe("Please consider removing this mod from your server installation");
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
+    @SideOnly(Side.CLIENT)
+    public void clientPreInit(FMLPreInitializationEvent event)
+    {
+        config = new EnchantIconsConfiguration(event.getSuggestedConfigurationFile());
+    }
+
+    @EventHandler
+    @SideOnly(Side.CLIENT)
+    public void clientInit(FMLInitializationEvent event)
     {
         MinecraftForgeClient.registerItemRenderer( Item.enchantedBook.itemID, new EnchantIconsRenderer() );
-        logger.log(Level.INFO, "Loaded version %s", VERSION);
+        LOG.log(Level.INFO, "Loaded version %s", VERSION);
     }
 }
